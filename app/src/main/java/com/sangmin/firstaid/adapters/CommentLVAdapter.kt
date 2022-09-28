0f29c42134ea8d4c9a24e8c4136f1bc0351cca35
박 상 민 <dhwkffks7@naver.com>
2022-09-28 15:34:29 +0900

    댓글 만들기 및 생성2

--- app/src/main/java/com/sangmin/firstaid/adapters/CommentLVAdapter.kt
+++ app/src/main/java/com/sangmin/firstaid/adapters/CommentLVAdapter.kt
@@ -1,15 +1,14 @@
 package com.sangmin.firstaid.adapters
 
-import android.graphics.Color
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.sangmin.firstaid.R
 import com.sangmin.firstaid.data.CommentModel
-import com.sangmin.firstaid.utils.FBAuth
+
 
 class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapter() {
     override fun getCount(): Int {
@@ -38,14 +37,8 @@ class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapte
                 .inflate(R.layout.comment_list_item, parent, false)
         }
 
-
-
-
         val title = view?.findViewById<TextView>(R.id.titleTxt)
         val time = view?.findViewById<TextView>(R.id.timeTxt)
-
-
-
 
         title!!.text = commentList[position].commentTitle
         time!!.text = commentList[position].commentTime
